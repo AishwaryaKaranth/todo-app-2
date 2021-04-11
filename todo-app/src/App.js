17965202e2bfd3ee485714651c1ee3ee850c071e
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-11 21:54:36 +0530

    connected to firebase

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,45 @@
 import React from "react";
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import {useState} from "react";
+import firebase from "firebase";
+import {db} from "./firebase_config";
 import './App.css';
 
 function App() {
-  const [todo, setTodo]=useState("")
-  const addItem=()=>{
+  const [todos, setTodo]=useState("")
+
+  const addItem=(e)=>{
+    e.preventDefault();
     
+    db.collection("todo").add({
+      inprogress:true,
+      timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+      todo:todos,
+    });
+
+    setTodo("");
   }
   return (
     <div className="App">
       <h1>Eisenhower's Matrix</h1>
-      <TextField 
-        id="standard-basic" 
-        label="Standard" 
-        value={todo}
-        onChange={(e)=>{
-          setTodo(e.target.value)
-        }}
-      />
+      <form>
+        <TextField 
+          id="standard-basic" 
+          label="Standard" 
+          value={todos}
+          onChange={(e)=>{
+            setTodo(e.target.value)
+          }}
+        />
+        <Button 
+          variant="contained" 
+          color="primary"
+          onClick={addItem}
+        >
+          Add
+        </Button>
+      </form>
     </div>
   );
 }
