c4881926c61bdf96e800a8efe062d129adbcfff5
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-11 22:19:42 +0530

    render todo list on frontend

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {useState} from "react";
+import {useEffect} from "react";
 import firebase from "firebase";
 import {db} from "./firebase_config";
 import './App.css';
 
 function App() {
   const [todos, setTodo]=useState("")
-
+  const [list, setList]=useState([])
   const addItem=(e)=>{
     e.preventDefault();
     
@@ -19,6 +20,23 @@ function App() {
     });
 
     setTodo("");
+  }
+
+  useEffect(()=>{getItem();
+  },[]);
+
+  const getItem=()=>{
+    db.collection("todo").onSnapshot(function(querySnapshot){
+      
+        setList(
+          querySnapshot.docs.map((doc)=>({
+            id:doc.id,
+            todo:doc.data().todo,
+            inprogress:doc.data().inprogress,
+          }))
+        );
+      });
+    
   }
   return (
     <div className="App">
@@ -40,6 +58,9 @@ function App() {
           Add
         </Button>
       </form>
+      {list.map((todo) => (
+        <p>{todo.todo}</p>
+      ))}
     </div>
   );
 }
