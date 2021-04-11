7e96d058fe1f4e78c037768c1d9417d68ca8ac7e
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2021-04-11 18:05:06 +0530

    intial setup

---
+++ todo-app/src/firebase_config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAbT_GiaeFh9cZASyYTzVH8iKUbcARPXmc",
+    authDomain: "todo-325cc.firebaseapp.com",
+    projectId: "todo-325cc",
+    storageBucket: "todo-325cc.appspot.com",
+    messagingSenderId: "344011275129",
+    appId: "1:344011275129:web:cb8c52bc9fd006aa414da8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ 
