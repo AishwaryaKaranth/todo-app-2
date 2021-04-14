750fac3769226440b291a15c7256cc4ad3f989bd
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2021-04-14 20:41:43 +0530

    Update firebase_config.js

--- todo-app/src/firebase_config.js
+++ todo-app/src/firebase_config.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAbT_GiaeFh9cZASyYTzVH8iKUbcARPXmc",
+    apiKey: "",
     authDomain: "todo-325cc.firebaseapp.com",
     projectId: "todo-325cc",
     storageBucket: "todo-325cc.appspot.com",
-    messagingSenderId: "344011275129",
-    appId: "1:344011275129:web:cb8c52bc9fd006aa414da8"
+    messagingSenderId: "",
+    appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
